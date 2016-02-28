063bb2c11b4190c67986b19d2f6c14907329b27a
Jan Hünermann <j.huenermann@icloud.com>
2016-02-28 02:02:52 +0100

    Added parameter export function in NetworkStructure, etc

--- test/time.js
+++ test/time.js
@@ -11,38 +11,44 @@ var layers = [
 var net = new nn.Network({
     layers: layers,
     learner: {
-        method: 'adadelta',
+        method: 'sgd',
+        rate: 0.01,
+        momentum: 0.9,
         timespan: 5,
         decay: { l1: 0, l2: 1e-5 }
     }
 });
 
+/* learns to delay a number for 4 time steps */
 console.time("nn");
 var k = 0;
-for (var i = 0; i < 100000; i++) {
+for (var i = 0; i < 500000; i++) {
     if (Math.random() < 0.3) {
     	var seq = [];
-        seq.push(Math.round(net.forward([1])[0]));
+        seq.push(net.forward([1])[0]);
         net.backward([0]);
 
         for (var t = 0; t < 3; t++) {
-            seq.push(Math.round(net.forward([0])[0]));
+            seq.push(net.forward([0])[0]);
             net.backward([0]);
         }
 
-        seq.push(Math.round(net.forward([0])[0]));
+        seq.push(net.forward([0])[0]);
         var loss = net.backward([1]);
 
-        if (k % 1000 === 0) {
-        	console.log(seq);
-        	console.log(loss);
+        if (k % 10000 === 0) {
+            console.log('run ' + k);
+        	console.log(seq.toString());
+        	console.log('loss: ' + loss);
         }
         ++k;
     }
 
     net.forward([0]);
+    net.backward([0]);
 }
 
+console.log(JSON.stringify(net.layers.parameters()));
 var stats = net.layers.stats();
 console.log('100,000 iterations of network with ' + stats.parameters + ' parameters and ' + stats.nodes + ' nodes');
 console.timeEnd("nn");
