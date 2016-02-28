8514f8c5df83e1404e7488b0d388716c7485ec82
Jan Hünermann <j.huenermann@icloud.com>
2016-02-28 03:52:37 +0100

    Web worker and readme changes
    Web worker demo, export lib to ww, changes to readme, little change to
    LSTM

--- readme.md
+++ readme.md
@@ -5,18 +5,23 @@ This library is intended to be a generalized model of neural networks, which ena
 I decided to write this library in JavaScript because it basically runs on every Internet-enabled device, enabling it to run on the most kinds of devices. Another factor was that it can be either run on the web, which means there is no setup required to execute it, or on the desktop with the help of NodeJS. In addition JavaScript is one of the easiest to learn languages.
 
 ## Features
-- A generalized BPTT (back-propagation through time)
+- A generalized BPTT (back-propagation through time) algorithm for use in either recurrent or feed-forward models
 - Abstract layer-network model
-- Network and data are seperated
+- Network model & network data-flow are seperated
 
 ## Current layer implementations
 - Fully connected layer (Dot)
 - Convolutional layer
-- Long Short-Term memory layers
+- Long Short-Term memory layer
 - Softmax and hierarchical softmax layer
 - Non-linear layers like sigmoid, tanh and rectifier
 - Regression and input layer
 - Drop-out layer
+
+# Future
+- Embedding layer
+- More & better demos (MNIST demo, CIFAR-10 demo, char-rnn demo, performance tests, etc)
+- Maybe native web worker support (especially Chrome is like 2x faster)
 
 ## Acknowledgments
 I would like to thank @karpathy (Andrej Karpathy) for his rigorous work on JavaScript deep-learning libraries, which I took inspiration and help from. (In parts you can see that in the code)
--- test/time.js
+++ test/time.js
@@ -1,5 +1,12 @@
-require('console-stamp')(console, 'HH:MM:ss.l');
-var nn = require('../build/nn.js');
+var nn = null;
+
+if (typeof module === "undefined" || typeof module.exports === "undefined") {
+    self.importScripts('../build/nn.js');
+    nn = nnjs;
+} else {
+    require('console-stamp')(console, 'HH:MM:ss.l');
+    nn = require('../build/nn.js');
+}
 
 var layers = [
     { type: 'input', size: nn.Size3(1, 1, 1) },
@@ -13,7 +20,7 @@ var net = new nn.Network({
     learner: {
         method: 'adadelta',
         timespan: 5,
-        decay: { l1: 0, l2: 1e-5 }
+        decay: { l2: 1e-5 }
     }
 });
 
@@ -22,7 +29,7 @@ console.time("nn");
 var k = 0;
 for (var i = 0; i < 500000; i++) {
     if (Math.random() < 0.3) {
-    	var seq = [];
+        var seq = [];
         seq.push(net.forward([1])[0]);
         net.backward([0]);
 
@@ -35,9 +42,15 @@ for (var i = 0; i < 500000; i++) {
         var loss = net.backward([1]);
 
         if (k % 10000 === 0) {
-            console.log('run ' + k);
-        	console.log(seq.toString());
-        	console.log('loss: ' + loss);
+            if (typeof postMessage !== 'undefined') {
+                postMessage('run ' + k);
+                postMessage(seq.toString());
+                postMessage('loss: ' + loss);
+            } else {
+                console.log('run ' + k);
+                console.log(seq.toString());
+                console.log('loss: ' + loss);
+            }
         }
         ++k;
     }
